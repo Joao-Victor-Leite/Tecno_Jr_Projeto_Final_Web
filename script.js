0c8295dcc2e0b76b8552229ce0033c6c1bbe987a
João Victor Leite <101682973+Joao-Victor-Leite@users.noreply.github.com>
2022-07-19 10:50:42 -0300

    Atualização css

--- script.js
+++ script.js
@@ -1,60 +1,90 @@
+/*  ATUALIZAÇÕES A FAZER
+    - fazer funcoes para reduzir as variaveis globias
+    - renomear as variaveis
+    - polir as funcoes
+    - identar e lembrar de upar no github
+    - IMPORTANTE: fazer uma função de restaurar os padroes das sprites
+    - Fazer um contador de tentativas
+    - (termo) mostrar as letras certas caso a pessoa erre
+*/
+
 //varáveis e constantes para a função pokeRandom
 const botaoStart = document.querySelector("#startQuiz")
 const botaoCmp = document.querySelector("#confirmQuiz")
-var pokeId //variavel que receberá um id aleatório dentro do escopo selecionado
-
-//variáveis e constantes parar a função compara
 const userAnswer = document.querySelector("#userInput") //armazena a resposta do usuário
-var pokeComp //variavel que armazena o nome do pokemon
+var pokeComp
 
-/* TESTE PARA PEGAR OS VALORES DO SELECT 
-var select = getElementById('genSelect')
-var value = select.options[select.selectedIndex].value;
-*/
+//Função funcionando
+function switchGen(gen){
+    gen = Number(gen); //transforma a string gen em número
+    switch(gen){
+        case 1:
+            return Math.floor(Math.random() * 151 + 1);
 
+        case 2:
+            return Math.floor(Math.random() * 251 + 1);
 
-/*  Função que:
-        - gera um numero aleatório restringido a geração informada pelo usuário; (talvez fazer função pra isso)
-        - usa esse numero na url da API para resgatar um pokemon no intervalo;
-        - gera a imagem do pokemon na tela 
-        - armazena o nome do pokemon selecionado na variável pokeComp;
-    Não recebe parametros;
-    É chamada através do botão de id "botaoStart";
-*/
-function pokeRandom(){
-    pokeId = Math.floor(Math.random() * 905 + 1);
-    let url = 'https://pokeapi.co/api/v2/pokemon/'+pokeId;
-    fetch(url)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            //trabalhar os dados
-            console.clear();
-            //
-            console.log(data);
-            pokeComp = data['name'];
-            userAnswer.value = "";
-            document.getElementById("nome").innerHTML = data['name'];
-            let img = data['sprites']['front_default'];
-            document.getElementById("pokeImg").setAttribute('src', img);
-            document.getElementById("numero").innerHTML = data['id'];
-        })
-        .catch((erro) => {
-            console.log("Erro: "+ erro);
-        });
+        case 3:
+            return Math.floor(Math.random() * 386 + 1);
+
+        case 4:
+            return Math.floor(Math.random() * 493 + 1);
+
+        case 5:
+            return Math.floor(Math.random() * 649 + 1);
+
+        case 6:
+            return Math.floor(Math.random() * 719 + 1);
+
+        case 7:
+            return Math.floor(Math.random() * 785 + 1);
+
+        case 8:
+            return Math.floor(Math.random() * 898 + 1);
+
+        case 9:
+            return Math.floor(Math.random() * 905 + 1);
+        
+        default:
+            console.log("Deafult");
+            return 1;
+    }
 }
 
-/*  Função que confere o input do nome do pokemon inserido pelo usuário e o nome do pokemon vindo da API,    alertando ao usuário se sua tentativa foi bem sucedida ou não;
-    Não recebe parametro;
-    É chamada através do botão de id "botaoCmp";
-*/ 
+function pokeRandom(){
+    var select = document.getElementById('genSelect');
+    var gen = select.options[select.selectedIndex].value;
+    var pokeId = switchGen(gen); //variavel que receberá um id aleatório dentro do escopo selecionado
+    let url = 'https://pokeapi.co/api/v2/pokemon/'+pokeId;
+    fetch(url)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        userAnswer.value = "";
+        pokeComp = data['name']; //variavel que armazena o nome do pokemon
+        //document.getElementById("nome").innerHTML = data['name']; //altera o h2 para o nome do pokemon
+        let img = data['sprites']['front_default'];
+        document.getElementById("pokeImg").classList.remove("brightNormal"); //remove o brightness normal do sprite
+        document.getElementById("pokeImg").setAttribute('src', img);
+        document.getElementById("numero").innerHTML = data['id'];
+
+    })
+    .catch((erro) => {
+        console.log("Erro: "+ erro);
+    });
+}
+
 function compara(){
+    let img = document.getElementById('pokeImg');
     if(userAnswer.value == pokeComp){
         alert(`Parabéns`);
     }else{
         alert(`Muito burro filho`);
     }
+    img.classList.toggle('brightNormal');
+
+    //document.createElement();
 }
 
 botaoStart.addEventListener("click", pokeRandom);
