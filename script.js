f31604912ecb9b690ad034ef0eeeaad489af5042
João Victor Leite <101682973+Joao-Victor-Leite@users.noreply.github.com>
2022-07-31 05:11:23 -0300

    favicon, polimento do js, finalizacao do css

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
-/*  ATUALIZAÇÕES A FAZER
-    - fazer funcoes para reduzir as variaveis globias
-    - renomear as variaveis
-    - polir as funcoes
-    - identar e lembrar de upar no github
-    - IMPORTANTE: fazer uma função de restaurar os padroes das sprites
-    - Fazer um contador de tentativas
-    - (termo) mostrar as letras certas caso a pessoa erre
-    - cor bonita: #1d2b3a
-    */
-
 //varáveis e constantes para a função pokeRandom
 const botaoStart = document.querySelector("#startQuiz")
 const botaoCmp = document.querySelector("#confirmQuiz")
 const userAnswer = document.querySelector("#userInput") //armazena a resposta do usuário
 var pokeComp
 
-//Função funcionando
+/*A partir da escolha do usuário no select, um valor será recebido para a função. 
+Nela, há um switch case referente as gerações que retorna um valor aleatorio atraves 
+da funcao Math.random dentro dos id dos pokemons referentes ao escopo escolhido 
+(a partir da primeira geração até a escolhida)*/
 function switchGen(gen){
     gen = Number(gen); //transforma a string gen em número
     switch(gen){
@@ -52,6 +44,12 @@ function switchGen(gen){
     }
 }
 
+/*Função que é acionada através do botão botaoStart, onde chama a função 
+switchGen no qual armazena um numero para ser anexado a url da API, 
+gerando os dados de um pokemon da pokedex. Então sua imagem (sprite) 
+é revelada ao usuário sem brilho e é esperado sua interação no input 
+text para o segmento do programa. Há também um tratamento para toda 
+vez que esta função seja chamada, seja limpo o campo do input anterior.*/
 function pokeRandom(){
     var select = document.getElementById('genSelect');
     var gen = select.options[select.selectedIndex].value;
@@ -64,7 +62,6 @@ function pokeRandom(){
     .then((data) => {
         userAnswer.value = "";
         pokeComp = data['name']; //variavel que armazena o nome do pokemon
-        //document.getElementById("nome").innerHTML = data['name']; //altera o h2 para o nome do pokemon
         let img = data['sprites']['front_default'];
         document.getElementById("pokeImg").classList.remove("brightNormal"); //remove o brightness normal do sprite
         document.getElementById("pokeImg").setAttribute('src', img);
@@ -76,16 +73,20 @@ function pokeRandom(){
     });
 }
 
+/*Função que é chamada através do botão botaoCmp, recebe a resposta do 
+usuário através do input text e esta é comparada com o nome do pokemon, 
+emitindo uma mensagem de alerta caso o usuário tenha acertado ou não. 
+Após a resposta, a imagem do pokemon é revelada e ampliada ao usuário, 
+então ele pode assim repetir o processo mudando ou nao a opção do select.*/
 function compara(){
     let img = document.getElementById('pokeImg');
     if(userAnswer.value == pokeComp){
         alert(`Parabéns`);
     }else{
-        alert(`Muito burro filho`);
+        alert(`Mais sorte na próxima`);
     }
     img.classList.toggle('brightNormal');
 
-    //document.createElement();
 }
 
 botaoStart.addEventListener("click", pokeRandom);
